c2b718b55a05548ad8c52e38f923b8fd0072e89f
Lusilud <ZHANGLUSILUD@GMAIL.COM>
2026-06-25 02:32:57 +0800

    Add DFR private header

---
+++ SpotifyOnTouchbar/Sources/DFRPrivate/DFRPrivateHeader.h
@@ -0,0 +1,10 @@
+#ifndef DFRPrivateHeader_h
+#define DFRPrivateHeader_h
+
+#import <Cocoa/Cocoa.h>
+
+// DFRFoundation private framework function declarations
+extern void DFRElementSetControlStripPresenceForIdentifier(NSTouchBarItemIdentifier identifier, BOOL present);
+extern void DFRSystemModalShowsCloseBoxWhenFrontMost(BOOL show);
+
+#endif /* DFRPrivateHeader_h */
